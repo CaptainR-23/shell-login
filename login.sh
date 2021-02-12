320943db670a71bba4264101fc2d743375a0bb55
CaptainR-23 <78982722+CaptainR-23@users.noreply.github.com>
2021-02-13 02:22:37 +0530

    login.sh

--- login.sh
+++ login.sh
@@ -366,7 +366,7 @@ echo ""
 echo ""
 echo ""
 echo ""
-			figlet By Punit | lolcat
+			figlet By CaptainR | lolcat
 echo ""
 echo "" 
 echo "               "[██████████████████████ 100%]
@@ -403,7 +403,7 @@ echo -e "\e[1;32m
 
 
 \e[1;35m
-[+] Author : ♣♠Yours Punit♠|♣
+[+] Author : ♣♠Rahul Roy♠|♣
 [+] Team : Hackers
 [+] Github : https://www.github.com/Punit-art
 \e[0m"
